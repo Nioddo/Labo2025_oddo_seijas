81f4b5c43e3f61a49d82479c17682e03caa3d4ec
Nioddo <167439680+Nioddo@users.noreply.github.com>
2025-03-12 09:47:56 -0300

    hacemos el 3

---
+++ ejer-2-repaso.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Productos{
+    string nombre;
+    int cod_barra;
+    string marca;
+    float precio;
+    bool fecha_v;
+}
+void CargarMatriz(matriz[3][4]){
+    
+};
+int main() {
+    int matriz[3][4];
+}
