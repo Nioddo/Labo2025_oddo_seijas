68fb7c2c66569313d9746db7b037a73b9fd945f0
Nioddo <167439680+Nioddo@users.noreply.github.com>
2025-03-19 09:28:42 -0300

    falta terminar el 5 queda poco

--- labo oddo seijas/ejer-5-repaso.cpp
+++ labo oddo seijas/ejer-5-repaso.cpp
@@ -29,20 +29,66 @@ cin>>aux.precio;
 cout<<"cantidad de ventas del producto"<<;
 cin>>aux.cantidad_de_ventas;
 cout<<"categoria del producto"
+cin>>aux.categoria;
 cout<<"tiene promo(si/no)"<<;
 cin>>aux.promo;
 productos.push_back(aux);
 }
 }
 void reg_pedido(vector <producto> productos, vector <pedido> pedidos){
-pedido aux;
+pedido pedido;
+float precio=0, valor total=0;
+string siono="si";
+string nombre;
 cout<<"nombre del cliente"<<;
 cin>>aux.nombre_cliente;
-cout<<"pene"<<;
-
+cout<<"que fecha es hoy?(ej=**/**/****)"<<endl;
+cin>>pedido.fecha
+cout<<"producto requerido"<<;
+cin>>nombre;
+pedido.nombre_producto.push_back(nombre);
+cout<<"quiere pedir algo mas(si/no)"<<endl;
+cin>>siono;
+while (siono=="si")
+{
+cout<<"producto requerido"<<;
+cin>>nombre;
+pedido.nombre_producto.push_back(nombre);
+for(int i=0; i<productos.size i++){
+if (nombre==productos[i].nombre_producto)
+{
+    precio=productos[i].precio;
+    if (productos[i].promo=="si")
+    {
+        precio=precio*0.8;
+    }
+    valor_total+=precio
+}
+else{
+    cout<<"no se encontro el producto"<<endl;
+}
+cout<<"quiere pedir algo mas(si/no)"<<endl;
+cin>>siono;
+}
+}
+pedido.precio_total=valor_total;
+pedidos.push_back(pedido);
+}
+void mostrar_producto(vector <pedido> pedidos){
+    string nombre;
+    cout<<"nombre del cliente a buscar el pedido"<<endl;
+    cin>>nombre;
+for(int i=0; i<productos.size i++){
+if (nombre==productos[i].nombre_producto)
+{
+cout<<"nombre del cliente:"<<productos[i].nombre_producto
+}
+else{
+    cout<<"no se encontro el producto"<<endl;
+}
 }
 int main()
 {
 vector <producto> productos;
-
+    
 }
