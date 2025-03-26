f1080e985cf4b0861d6c613f2e6a7d287f712a06
Nioddo <167439680+Nioddo@users.noreply.github.com>
2025-03-26 09:18:22 -0300

    falta terminar el 6

--- labo oddo seijas/ejer-5-repaso.cpp
+++ labo oddo seijas/ejer-5-repaso.cpp
@@ -1,94 +1,196 @@
-include <iostream>
+#include <iostream>
 #include <vector>
-using namespace std ;
-
-struct producto{
-    string nombre;
-    string categoria;
-    float precio;
-    int cantidad_de_ventas;
-    string promo;
-}
-struct pedido{
-    string nombre_cliente;
-    vector <string> nombre_producto;
-    int cantidad_productos;
-    float precio_total;
-    string fecha;
-}
-void calculo( string nombre_producto, vector <producto> productos, vector <pedido> pedidos){
-    for(int i=0; i<)
-}
-void reg_pedido(vector <producto> productos){
-producto aux;
-for(int i=0; i=<20;i++){
-cout<<"ingrese nombre del producto"<<;
-cin>>aux.nombre;
-cout<<"precio del producto"<<;
-cin>>aux.precio;
-cout<<"cantidad de ventas del producto"<<;
-cin>>aux.cantidad_de_ventas;
-cout<<"categoria del producto"
-cin>>aux.categoria;
-cout<<"tiene promo(si/no)"<<;
-cin>>aux.promo;
-productos.push_back(aux);
-}
-}
-void reg_pedido(vector <producto> productos, vector <pedido> pedidos){
-pedido pedido;
-float precio=0, valor total=0;
-string siono="si";
+ 
+using namespace std;
+ 
+struct Producto{
+ 
 string nombre;
-cout<<"nombre del cliente"<<;
-cin>>aux.nombre_cliente;
-cout<<"que fecha es hoy?(ej=**/**/****)"<<endl;
-cin>>pedido.fecha
-cout<<"producto requerido"<<;
-cin>>nombre;
-pedido.nombre_producto.push_back(nombre);
-cout<<"quiere pedir algo mas(si/no)"<<endl;
-cin>>siono;
-while (siono=="si")
-{
-cout<<"producto requerido"<<;
-cin>>nombre;
-pedido.nombre_producto.push_back(nombre);
-for(int i=0; i<productos.size i++){
-if (nombre==productos[i].nombre_producto)
-{
-    precio=productos[i].precio;
-    if (productos[i].promo=="si")
-    {
-        precio=precio*0.8;
-    }
-    valor_total+=precio
+bool promocion;
+int cantventas;
+float precio;
+string categoria;
+ 
+};
+ 
+struct Fecha{
+    int dia;
+    int mes;
+    int año;
+};
+ 
+struct Pedido{
+    string cliente;
+    vector <Producto> productos;
+    vector <Fecha> fechas;
+    int total=0;
+};
+ 
+void ingresarprod (vector <Producto> &productos){
+Producto producto;
+string opcion;
+cout<<"ingrese el nombre del producto: "<<endl;
+cin>>producto.nombre;
+cout<<endl;
+ 
+cout<<"¿tiene descuento (si/no)?"<<endl;
+cin>>opcion;
+cout<<endl;
+if (opcion == "si"){
+    producto.promocion=true;
 }
 else{
-    cout<<"no se encontro el producto"<<endl;
+    producto.promocion=false;
+ 
 }
-cout<<"quiere pedir algo mas(si/no)"<<endl;
-cin>>siono;
+ 
+producto.cantventas=0;
+ 
+cout<<"ingrese el precio del producto: "<<endl;
+cin>>producto.precio;
+cout<<endl;
+ 
+cout<<"ingrese la categoria del producto: "<<endl;
+cin>>producto.categoria;
+cout<<endl;
+ 
+if (producto.promocion==true){
+    producto.precio=producto.precio-producto.precio*0.2;
+}
+ 
+productos.push_back(producto);
+}
+ 
+ 
+void ingresarped (vector <Producto> &productos , vector <Fecha> &fechas, vector <Pedido> &pedidos){
+Pedido pedido;
+Fecha fecha;
+string opcion="si";
+ 
+cout<<"ingrese su nombre: "<<endl;
+cin>>pedido.cliente;
+cout<<endl;
+ 
+ 
+while (opcion=="si"){
+cout<<"ingrese el nombre de los productos que deseea: "<<endl;
+string nomprod;
+cin>>nomprod;
+cout<<endl;
+for(int i=0; i<productos.size(); i++){
+if (nomprod==productos[i].nombre){
+    productos[i].cantventas++;
+    pedido.productos.push_back(productos[i]);
 }
 }
-pedido.precio_total=valor_total;
+ 
+cout<<"quieres ingresar otro producto?"<<endl;
+cin>>opcion;
+cout<<endl;
+ 
+}
+ 
+cout<<"ingrese el dia"<<endl;
+cin>>fecha.dia;
+cout<<endl;
+cout<<"ingrese el mes"<<endl;
+cin>>fecha.mes;
+cout<<endl;cout<<"ingrese el año"<<endl;
+cin>>fecha.año;
+cout<<endl;
+ 
+fechas.push_back(fecha);
+pedido.fechas.push_back(fecha);
+for(int i=0;i<pedido.productos.size();i++){
+ 
+pedido.total+=pedido.productos[i].precio;
+cout<<"El precio va en:"<<pedido.total<<endl;
+ 
+}
 pedidos.push_back(pedido);
 }
-void mostrar_producto(vector <pedido> pedidos){
-    string nombre;
-    cout<<"nombre del cliente a buscar el pedido"<<endl;
-    cin>>nombre;
-for(int i=0; i<productos.size i++){
-if (nombre==productos[i].nombre_producto)
+ 
+ 
+void busqueda(vector <Pedido> pedidos){
+string nom;
+ 
+cout<<"para buscar un pedido ingrese el nombre del cliente que lo pidio"<<endl;
+cin>>nom;
+cout<<endl;
+ 
+for(int i=0; i<pedidos.size();i++)
 {
-cout<<"nombre del cliente:"<<productos[i].nombre_producto
+    if(nom==pedidos[i].cliente){
+        for(int k=0; k<pedidos[i].productos.size();k++)
+        {
+            cout<<pedidos[i].productos[k].nombre<<endl;
+            cout<<pedidos[i].productos[k].precio<<endl;
+            
+    }
+        cout<<"total del pedido: "<<pedidos[i].total<<endl;
+        return;
 }
-else{
-    cout<<"no se encontro el producto"<<endl;
 }
+cout<<"no se encontro el pedido"<<endl;
+return;
 }
-int main()
-{
-vector <producto> productos;
+ 
+void ordenar(vector<Producto> &productos) {
+    int n = productos.size();
+ 
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (productos[j].cantventas < productos[j + 1].cantventas) {
+                Producto temp = productos[j];
+                productos[j] = productos[j + 1];
+                productos[j + 1] = temp;
+            }
+        }
+    }
+ 
+    cout << "Productos ordenados por cantidad de ventas: ";
+    for (Producto producto : productos) {
+        cout << "Nombre: " << producto.nombre <<endl;
+          cout   << "Ventas: " << producto.cantventas <<endl;
+          cout   << "Precio: " << producto.precio <<endl;
+        cout  << "Categoria: " << producto.categoria<<endl;
+    }
+}
+ 
+int main (){
+vector <Pedido> pedidos;
+vector <Producto> productos;
+vector <Fecha> fechas;
+ 
+int opcion;
+  do{     
+    cout << "1 - ingresar producto" << endl;
+    cout << "2 - ingresar pedido" << endl;
+    cout << "3 - busca pedido" << endl;
+    cout << "4 - productos ordenados" << endl;
+    cout<<"Elija una opcion"<<endl;
+    cin>>opcion;
     
+    switch (opcion)
+        {
+        case 1:
+            ingresarprod(productos);
+            break;
+        case 2:
+            ingresarped(productos,fechas,pedidos);
+            break;
+        case 3:
+            busqueda(pedidos);
+            break;
+        case 4:
+            ordenar(productos);
+            break;
+            case 5:
+                cout << "Saliendo del programa..." << endl;
+                break;
+            default:
+                cout << "Opción no válida. Intenta nuevamente." << endl;
+        }
+    } while (opcion != 5);
+ 
 }
---
+++ labo oddo seijas/ejer-6-repaso.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std ;
+
+struct Equipo{
+    string nombre;
+    int goles_a_favor;
+    int goles_en_contra;
+    int posicion;
+    string estadio;
+    
+
+
+}
