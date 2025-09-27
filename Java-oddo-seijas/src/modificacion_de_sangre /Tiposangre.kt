d174f7be4958d3347dd5e2d6a059d1193a3030f0
Nioddo <167439680+Nioddo@users.noreply.github.com>
2025-09-27 14:53:22 -0300

    Delete Java-oddo-seijas/src/modificacion_de_sangre  directory

--- Java-oddo-seijas/src/modificacion_de_sangre /Tiposangre.kt
+++
@@ -1,5 +0,0 @@
-package `modificacion_de_sangre `
-
-enum class Tiposangre {
-    A+, B+, AB+, O+, A-, B-, AB-, O-
-}
--- Java-oddo-seijas/src/modificacion_de_sangre /paciente.java
+++
@@ -1,7 +0,0 @@
-import Personas.PersonaUseAbs;
-
-public class paciente extends PersonaUseAbs{
-
-private genero genero;
-private Tiposangre tipoSangre;
-}
